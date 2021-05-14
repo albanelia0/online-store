6aeae2e62e258c4ab1dfb3aa43f780e84398e865
Alba <albanelia0@gmail.com>
2021-05-14 12:52:14 +0200

    Add cart image and eyes image to the card

--- src/component/Card/index.js
+++ src/component/Card/index.js
@@ -6,6 +6,14 @@ const Card = ({title, price, imgSrc}) => {
       <img alt="card" className="card__img" src={imgSrc} />
       <span className="card__title">{title}</span>
       <span className="card__price">{price}</span>
+      <div className="cart-wrapper">
+        <button className="cart-wrapper__button-img cart-wrapper__button-style">
+          <img className="cart-wrapper__img" alt="carrito" src="/img/carrito.svg" />
+        </button>
+        <button className="cart-wrapper__button-eyes cart-wrapper__button-style">
+          <img className="cart-wrapper__eyes" alt="ojo" src="/img/ojo.svg" />
+        </button>
+      </div>
     </div>
   )
 }
