1a0d68a0a6e798b44def6bd2ef754f1046c26023
Alba <albanelia0@gmail.com>
2021-05-15 13:48:24 +0200

    Add slider with swiper

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,25 @@
 import {useContext } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+
 import Breadcrumbs from "../../component/Breadcrumbs"
 import Card from "../../component/Card"
 import Layout from "../../component/Layout"
 
 import { Context } from "../../CartStoreContext"
-
 import "./index.scss"
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination,Navigation
+} from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Pagination,Navigation]);
 
 const Products = () => {
 
@@ -19,18 +33,30 @@ const Products = () => {
           <Breadcrumbs/>
         </div>
         <div className="products">
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={105}
+            slidesPerGroup={1}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            navigation={true}
+            className="mySwiper"
+          >
           {products?.map(({id, image, title, price }) => {
             return (
-              <div key={id}>
-                <Card
-                  handleToAddItemToCart={() => handleToAddItemToCart({id, image, title, price })}
-                  title={title}
-                  imgSrc={image}
-                  price={price}
-                />
-              </div>
+              <SwiperSlide>
+                <div key={id}>
+                    <Card
+                      handleToAddItemToCart={() => handleToAddItemToCart({id, image, title, price })}
+                      title={title}
+                      imgSrc={image}
+                      price={price}
+                    />
+                </div>
+              </SwiperSlide>
             )
           })}
+          </Swiper>
         </div>
       </Layout>
     </div>
