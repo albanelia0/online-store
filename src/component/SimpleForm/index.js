83e227d6f860bd5d959cb1c3629a97ced5ac6b52
Alba <albanelia0@gmail.com>
2021-05-15 17:54:43 +0200

    Add SimpleForm Component to basket page

---
+++ src/component/SimpleForm/index.js
@@ -0,0 +1,13 @@
+import "./index.scss"
+
+const SimpleForm = () => {
+  return (
+    <form className="simple-form">
+      <label className="simple-form__label">Código de descuento</label>
+      <input className="simple-form__input" type="text" />
+      <button className="simple-form__button">Aplicar</button>
+    </form>
+  )
+}
+
+export default SimpleForm
--- src/pages/Basket/index.js
+++ src/pages/Basket/index.js
@@ -6,6 +6,7 @@ import Layout from "../../component/Layout"
 
 import "./index.scss"
 import Resume from "../../component/Resume"
+import SimpleForm from "../../component/SimpleForm"
 
 const Basket = () => {
   const { itemsAdded, handleToSumTotalPrice, handleToDeleteItem } = useContext(Context)
@@ -16,8 +17,11 @@ const Basket = () => {
     <Layout>
       <div className="cart-wrapper">
         <CartItems handleToDeleteItem={handleToDeleteItem} allItems={itemsAdded.items}/>
-        <div>
+        <div className="cart-wrapper__resume-container">
           <Resume count={itemsAdded.count} price={totalPrice} />
+          <div className="cart-wrapper__form">
+            <SimpleForm />
+          </div>
         </div>
       </div>
     </Layout>
